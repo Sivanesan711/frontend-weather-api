7352a092c60f9ee52141adc3f9f449b2c3380d44
SIVANESAN-DHANIKACHALAM <61906309+Sivanesan711@users.noreply.github.com>
2020-06-13 06:05:22 +0530

    Add files via upload

--- frontend-weather-api/weather.js
+++ frontend-weather-api/weather.js
@@ -1,33 +1,26 @@
-var input = document.querySelector('.input_text');
-var main = document.querySelector('#showPlace');
-var temp = document.querySelector('.temp');
-var desc = document.querySelector('.desc');
-var clouds = document.querySelector('.clouds');
-var button = document.querySelector('.btn');
+var txt1 = document.getElementById("text1");
+var showPlace = document.getElementById("showPlace");
+var showDayAndTime = document.getElementById("showDayAndTime");
+var showTemperature = document.getElementById("showTemperature");
+var showWeather = document.getElementById("showWeather");
+var btn = document.getElementById("btn");
 
+button.addEventListener('click', function(showPlace){
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+txt1.value+'&appid=64cc008a818db50b99b5139189b2044d')
+.then(response => response.json())
+.then(data => {
+  var showPlaceValue = data['name'];
+  var showDayAndTimeValue = data['timezone'];
+  var showTemperatureValue = data['main']['temp'];
+  var showWeatherValue = data['weather'][0]['description'];
 
+  showPlace.innerHTML = showPlaceValue;
+  showDayAndTime.innerHTML = showDayAndTimeValue;
+  showWeather.innerHTML = showWeatherValue;
+  showTemperature.innerHTML = showTemperatureValue;
+  input.value ="";
 
-button.addEventListener('click', function(name) {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + input.value + '&appid=3d2f986f45064d5f6d21daa9109d5fb6')
-        .then(response => response.json())
-        .then(data => {
-            var tempValue = data['main']['temp'];
-            var nameValue = data['name'];
-            var descValue = data['weather'][0]['description'];
-            main.innerHTML = nameValue;
-            desc.innerHTML = descValue;
-            temp.innerHTML = tempValue;
-            today.innerHTML = today;
-            time.main = time;
+})
 
-            input.value = "";
-
-            var today = new Date();
-            var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-
-
-
-        })
-
-
+.catch(err => alert("Wrong city name!"));
 })
